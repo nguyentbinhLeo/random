0d42109540256764f8f69151e1d284b1d3cda33a
nguyentbinhLeo <128013059+nguyentbinhLeo@users.noreply.github.com>
2024-04-01 17:24:11 +0700

    boostrap-testing

---
+++ component.js
@@ -0,0 +1,18 @@
+function createCard(title, content, buttonText, buttonLink) {
+    // Create card element
+    var card = document.createElement('div');
+    card.className = 'col-md-4 mb-4';
+
+    // Create card HTML content
+    card.innerHTML = `
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">${title}</h5>
+                <p class="card-text">${content}</p>
+                <a href="${buttonLink}" class="btn btn-primary">${buttonText}</a>
+            </div>
+        </div>
+    `;
+
+    return card;
+}
